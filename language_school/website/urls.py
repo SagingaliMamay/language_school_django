1428817babec6be7598d6d2efe5313b8f1e52857
SagingaliMamay <sagingali.mamayev@gmail.com>
2022-07-18 23:37:12 +0200

    frontend of courses & course_details of pages is done

--- language_school/website/urls.py
+++ language_school/website/urls.py
@@ -5,4 +5,11 @@ from django.urls import path
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('course_details', views.course_details, name='course_details'),
+    path('courses', views.courses, name='courses'),
+    path('events', views.events, name='events'),
+    path('pricing', views.pricing, name='pricing'),
+    path('trainers', views.trainers, name='trainers'),
+
 ]
--- language_school/website/views.py
+++ language_school/website/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
 
 
-# Home page
+# All functions for all pages
 
 
 def home(request):
     return render(request, 'home.html', {})
 
-# About page
-
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def course_details(request):
+    return render(request, 'course_details.html', {})
+
+
+def courses(request):
+    return render(request, 'courses.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def events(request):
+    return render(request, 'events.html', {})
+
+
+def pricing(request):
+    return render(request, 'pricing.html', {})
+
+
+def trainers(request):
+    return render(request, 'trainers.html', {})
